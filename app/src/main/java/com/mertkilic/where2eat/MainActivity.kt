2fb54cc2dc97acb57f047dfd98eeaf145b146d46
Mert Kilic <mert.kilic@blacklane.com>
2020-06-20 15:32:56 +0200

    Init project

---
+++ app/src/main/java/com/mertkilic/where2eat/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mertkilic.where2eat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
---
+++ app/src/main/java/com/mertkilic/where2eat/Where2EatApp.kt
@@ -0,0 +1,16 @@
+package com.mertkilic.where2eat
+
+import com.mertkilic.where2eat.di.ApplicationModule
+import com.mertkilic.where2eat.di.Dagger
+import dagger.android.support.DaggerApplication
+
+class Where2EatApp : DaggerApplication() {
+
+  open val applicationModule by lazy { ApplicationModule(this) }
+
+  override fun onCreate() {
+    super.onCreate()
+  }
+
+  override fun applicationInjector() = Dagger.INJECTOR.create(this).applicationComponent
+}
